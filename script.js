c9377ea71c2b2e1af13ce2f0e5e790c43576ca2d
Ayah Safi <ayaadnansafi@gmail.com>
2023-10-23 23:17:20 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeTxt(btn) {
+    btn.innerText  = "Logout";
+}
+
+function removeElemnt(btn) {
+    btn.remove();
+}
+
+function displayMessage() {
+    alert("ninja was liked");
+
+}
